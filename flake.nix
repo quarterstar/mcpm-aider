3d2f676046c999a6cb5f97ca5315aaeec64b90b4
quarterstar <quarterstar@proton.me>
2026-05-25 00:00:00 +0000

    feat: setup flakes dev environment

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "A flake to build mcpm with native Draft 2020-12 schema validation support";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+      in
+      {
+        packages.default = pkgs.buildNpmPackage rec {
+          pname = "mcpm";
+          version = "58a9c35e4ee53b322cef5bd457fee3135d464257";
+
+          src = lib.cleanSource ./.;
+
+          npmDepsHash = "sha256-gqa0meuDq+359oYdgQX7kXcHqnZ1PIRn2423H1mKeTw=";
+          npmBuildScript = "build";
+          npmFlags = [ "--legacy-peer-deps" ];
+
+          postInstall = ''
+            echo "Installing compiled TypeScript artifacts..."
+            mkdir -p $out/lib/node_modules/${pname}/dist
+
+            if [ -d "dist" ]; then
+              cp -r dist/* $out/lib/node_modules/${pname}/dist/
+            elif [ -d "build" ]; then
+              mkdir -p $out/lib/node_modules/${pname}/build
+              cp -r build/* $out/lib/node_modules/${pname}/build/
+            fi
+          '';
+
+          nativeBuildInputs = [ pkgs.nodejs ];
+          buildInputs = [ pkgs.nodejs ];
+
+          meta = {
+            mainProgram = "mcpm-aider";
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.nodePackages.npm
+          ];
+        };
+      }
+    );
+}
